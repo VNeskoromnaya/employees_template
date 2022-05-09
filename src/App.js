1b3f5011181bfb2f0f4c27ae3c03c7ec214a3ba1
VNeskoromnaya <neskoromnaya.v@gmail.com>
2022-05-09 22:13:40 +0300

    add components and their css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import AppInfo from './components/app-info/app-info';
+import SearchPanel from './components/search-panel/search-panel';
+import AppFilter from './components/app-filter/app-filter';
+import EmployeesList from './components/employees-list/employees-list';
+import EmployeesAddForm from './components/employees-add-form/employees-add-form';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppInfo/>
+
+      <div className="search-panel">
+        <SearchPanel/>
+        <AppFilter/>
+      </div>
+
+      <EmployeesList/>
+
+      <EmployeesAddForm/>
     </div>
   );
 }
